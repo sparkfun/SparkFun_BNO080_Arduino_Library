d17b774badd4cb92f8517aa5ccc9e55b24a55b24
Nathan Seidle <nathan@sparkfun.com>
2019-08-29 11:56:00 -0600

    Change intPin to 255. Should fix issue 25

--- src/SparkFun_BNO080_Arduino_Library.h
+++ src/SparkFun_BNO080_Arduino_Library.h
@@ -132,7 +132,7 @@ const byte CHANNEL_GYRO = 5;
 class BNO080
 {
 public:
-	boolean begin(uint8_t deviceAddress = BNO080_DEFAULT_ADDRESS, TwoWire &wirePort = Wire, uint8_t intPin = NULL); //By default use the default I2C addres, and use Wire port, and don't declare an INT pin
+	boolean begin(uint8_t deviceAddress = BNO080_DEFAULT_ADDRESS, TwoWire &wirePort = Wire, uint8_t intPin = 255); //By default use the default I2C addres, and use Wire port, and don't declare an INT pin
 	boolean beginSPI(uint8_t user_CSPin, uint8_t user_WAKPin, uint8_t user_INTPin, uint8_t user_RSTPin, uint32_t spiPortSpeed = 3000000, SPIClass &spiPort = SPI);
 
 	void enableDebugging(Stream &debugPort = Serial); //Turn on debug printing. If user doesn't specify then Serial will be used.
